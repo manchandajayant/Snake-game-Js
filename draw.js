4cb48d51ae11cf7d44edfaa7e56542c86f5d681a
manchandajayant <maanjayant@gmail.com>
2020-06-06 13:01:41 +0200

    first

---
+++ draw.js
@@ -0,0 +1,28 @@
+const canvas = document.querySelector(".canvas");
+const ctx = canvas.getContext("2d");
+const scale = 10;
+const rows = canvas.height / scale;
+const columns = canvas.width / scale;
+var snake;
+
+(function setup() {
+  snake = new Snake();
+  fruit = new Fruit();
+
+  fruit.pickLocation();
+  console.log(fruit);
+  window.setInterval(() => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    fruit.draw();
+    snake.update();
+    snake.draw();
+  }, 250);
+})();
+
+window.addEventListener("keydown", (event) => {
+  console.log(event);
+  const direction = event.key.replace("Arrow", "");
+  console.log(direction);
+  snake.changeDirection(direction);
+});
+console.log("c", canvas);
---
+++ snake.js
@@ -0,0 +1,48 @@
+function Snake() {
+  this.x = 0;
+  this.y = 0;
+  this.xSpeed = scale * 1;
+  this.ySpeed = 0;
+  this.draw = function () {
+    ctx.fillStyle = "#000000";
+    ctx.fillRect(this.x, this.y, scale, scale);
+  };
+
+  this.update = () => {
+    this.x += this.xSpeed;
+    this.y += this.ySpeed;
+    if (this.x > canvas.width) {
+      this.x = 0;
+    }
+    if (this.y > canvas.height) {
+      this.y = 0;
+    }
+    if (this.x < 0) {
+      this.x = canvas.width;
+    }
+    if (this.y < 0) {
+      this.y = canvas.height;
+    }
+  };
+
+  this.changeDirection = (direction) => {
+    switch (direction) {
+      case "Up":
+        this.xSpeed = 0;
+        this.ySpeed = -scale * 1;
+        break;
+      case "Down":
+        this.xSpeed = 0;
+        this.ySpeed = scale * 1;
+        break;
+      case "Left":
+        this.xSpeed = -scale * 1;
+        this.ySpeed = 0;
+        break;
+      case "Right":
+        this.xSpeed = scale * 1;
+        this.ySpeed = 0;
+        break;
+    }
+  };
+}
